3cb33e245e719ae8e4c0db37658164729ada22b5
saliholoji <hdelete88@gmail.com>
2024-08-07 22:16:47 +0300

    Add project files.

---
+++ adb Manage/frmInstallApp.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace adb_Manage
+{
+    partial class frmInstallApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInstallApp));
+            this.lstPaths = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnInstall = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstPaths
+            // 
+            this.lstPaths.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lstPaths.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstPaths.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3});
+            this.lstPaths.FullRowSelect = true;
+            this.lstPaths.GridLines = true;
+            this.lstPaths.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lstPaths.LabelWrap = false;
+            this.lstPaths.Location = new System.Drawing.Point(12, 12);
+            this.lstPaths.MultiSelect = false;
+            this.lstPaths.Name = "lstPaths";
+            this.lstPaths.Scrollable = false;
+            this.lstPaths.Size = new System.Drawing.Size(186, 62);
+            this.lstPaths.TabIndex = 0;
+            this.lstPaths.TileSize = new System.Drawing.Size(168, 5);
+            this.lstPaths.UseCompatibleStateImageBehavior = false;
+            this.lstPaths.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Apk Name";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 102;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnInstall
+            // 
+            this.btnInstall.Location = new System.Drawing.Point(12, 219);
+            this.btnInstall.Name = "btnInstall";
+            this.btnInstall.Size = new System.Drawing.Size(186, 48);
+            this.btnInstall.TabIndex = 6;
+            this.btnInstall.Text = "Aktarımı Başlat";
+            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmInstallApp
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 283);
+            this.Controls.Add(this.lstPaths);
+            this.Controls.Add(this.btnInstall);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmInstallApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Install New Apps";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstPaths;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnInstall;
+    }
+}
---
+++ adb Manage/frmInstallApp.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ApkNet;
+using ApkNet.ApkReader;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace adb_Manage
+{
+    public partial class frmInstallApp : Form
+    {
+        public frmInstallApp()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;
+            pictureBox1.DragEnter += PictureBox1_DragEnter;
+            pictureBox1.DragDrop += PictureBox1_DragDrop;
+        }
+        Thread thr;
+        string dosyayolu = "";
+        #region Methods
+
+        void WaitForDevices()
+        {
+            MessageBox.Show("Bağlı bir Android cihaz yoksa cihazı bağlayana kadar beklenecektir, devam etmek için lütfen \"Tamam\" butonuna tıklayın!", "Cihaz Taranıyor...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            lstPaths.Invoke(() =>
+            {
+                lstPaths.Items[0].SubItems[1].Text = "Waiting...";
+                lstPaths.Items[0].ForeColor = Color.Maroon;
+            });
+            ADB.Shell(" wait-for-device");
+            GetConnectedDevices();
+        }
+
+
+        void GetConnectedDevices()
+        {
+            string Data = ADB.Shell(" devices");
+            string[] AllData = Data.Split('\n');
+            for (int i = 1; i < AllData.Length; i++)
+            {
+                Thread thr2 = new Thread(InstallApps);
+                thr2.IsBackground = true;
+                thr2.Start();
+            }
+        }
+
+        void InstallApps()
+        {
+            List<string> Paths = new List<string>();
+            lstPaths.Invoke(() =>
+            {
+
+                Paths.Add(dosyayolu);
+
+            });
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                lstPaths.Invoke(() =>
+                {
+                    lstPaths.Items[i].SubItems[1].Text = "Preparing...";
+                    lstPaths.Items[0].ForeColor = Color.Yellow;
+                });
+                File.Copy(Paths[i], "apkfile.apk", true);
+                lstPaths.Invoke(() =>
+                {
+                    lstPaths.Items[i].SubItems[1].Text = "Installing...";
+                    lstPaths.Items[i].ForeColor = Color.Blue;
+                });
+                string Result = ADB.Shell(" install apkfile.apk"); ;
+                lstPaths.Invoke(() =>
+                {
+                    lstPaths.Items[i].SubItems[1].Text = Result;
+                    if (Result != "Success")
+                    {
+                        MessageBox.Show(".APK yüklenirken sorun olmuş olabilir, gelen sonuç: " + Result, "İşlem sonuçlandı!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    lstPaths.Items[i].ForeColor = Color.Green;
+                });
+            }
+            btnInstall.Invoke(() => { btnInstall.Enabled = true; });
+            dosyayolu = "";
+
+        }
+        #endregion
+       
+        ListViewItem itm = new ListViewItem();
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "APK dosyaları (*.apk)|*.apk";
+            openFileDialog.Title = "APK Dosyası Seçin";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string apkDosyaYolu = openFileDialog.FileName;
+                dosyayolu = openFileDialog.FileName;
+
+                itm = new ListViewItem();
+                lstPaths.Items.Clear();
+
+                itm.Text = Path.GetFileName(dosyayolu);
+                itm.SubItems.Add("Ready");
+                lstPaths.Items.Add(itm);
+
+                ApkInfo infos = GetApkIcon(apkDosyaYolu);
+                if (infos == null)
+                {
+                    pictureBox1.Image = Image.FromFile(Application.StartupPath + @"\images\noicon.png");
+                    MessageBox.Show("APK dosyasından ikon alınamadı. Bu bir sorun değil, sadece bilgilendirmedir!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                try
+                {
+                    Bitmap icon = GetApkIconSon(apkDosyaYolu, infos.iconFileName[0]);
+                    if (icon != null)
+                    {
+                        pictureBox1.Image = icon;
+                    }
+                }
+                catch
+                {
+                    pictureBox1.Image = Image.FromFile(Application.StartupPath + @"\images\noicon.png");
+                    MessageBox.Show("APK dosyasından ikon alınamadı. Bu bir sorun değil, sadece bilgilendirmedir!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+        }
+
+
+        private Bitmap GetApkIconSon(string apkDosyaYolu, string yol)
+        {
+            try
+            {
+                using (var zipArchive = System.IO.Compression.ZipFile.OpenRead(apkDosyaYolu))
+                {
+                    var iconEntry = zipArchive.GetEntry(yol);
+                    if (iconEntry != null)
+                    {
+                        using (var stream = iconEntry.Open())
+                        {
+                            return new Bitmap(stream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                pictureBox1.Image = Image.FromFile(Application.StartupPath + @"\images\noicon.png");
+                MessageBox.Show("İkon alınırken bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return null;
+        }
+
+        private ApkInfo GetApkIcon(string path)
+        {
+
+            try
+            {
+                byte[] manifestData = null;
+                byte[] resourcesData = null;
+                using (var zip = new ZipInputStream(File.OpenRead(path)))
+                {
+                    using (var filestream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        var zipfile = new ICSharpCode.SharpZipLib.Zip.ZipFile(filestream);
+                        ICSharpCode.SharpZipLib.Zip.ZipEntry item;
+                        while ((item = zip.GetNextEntry()) != null)
+                        {
+                            if (item.Name.ToLower() == "androidmanifest.xml")
+                            {
+                                manifestData = new byte[50 * 1024];
+                                using (var strm = zipfile.GetInputStream(item))
+                                {
+                                    strm.Read(manifestData, 0, manifestData.Length);
+                                }
+
+                            }
+                            if (item.Name.ToLower() == "resources.arsc")
+                            {
+                                using (var strm = zipfile.GetInputStream(item))
+                                {
+                                    using (var s = new BinaryReader(strm))
+                                    {
+                                        resourcesData = s.ReadBytes((int)s.BaseStream.Length);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var apkReader = new ApkReader();
+                var info = apkReader.extractInfo(manifestData, resourcesData);
+
+                return info;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private void PictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            // Sadece dosyaları kabul edelim
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Mouse ikonunu değiştirelim
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void PictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Sürüklenen dosyaları alalım
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // İlk dosyanın yolunu MessageBox ile gösterelim
+            string firstFilePath = files[0];
+
+            // İlk dosyanın uzantısını alalım
+            string fileExtension = Path.GetExtension(firstFilePath);
+
+            // Uzantı .apk ise işlem yapalım
+            if (fileExtension.Equals(".apk", StringComparison.OrdinalIgnoreCase))
+            {
+                string apkDosyaYolu = firstFilePath;
+
+                dosyayolu = firstFilePath;
+
+                itm = new ListViewItem();
+                lstPaths.Items.Clear();
+
+                itm.Text = Path.GetFileName(dosyayolu);
+                itm.SubItems.Add("Ready");
+                lstPaths.Items.Add(itm);
+
+                ApkInfo infos = GetApkIcon(apkDosyaYolu);
+                if (infos == null)
+                {
+                    pictureBox1.Image = Image.FromFile(Application.StartupPath + @"\images\noicon.png");
+                    MessageBox.Show("APK dosyasından ikon alınamadı. Bu bir sorun değil, sadece bilgilendirmedir!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                try
+                {
+                    Bitmap icon = GetApkIconSon(apkDosyaYolu, infos.iconFileName[0]);
+                    if (icon != null)
+                    {
+                        pictureBox1.Image = icon;
+                    }
+                }
+                catch
+                {
+                    pictureBox1.Image = Image.FromFile(Application.StartupPath + @"\images\noicon.png");
+                    MessageBox.Show("APK dosyasından ikon alınamadı. Bu bir sorun değil, sadece bilgilendirmedir!", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bırakılan dosya bir APK dosyası değildir!", "APK Dosyası Bulunamadı");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dosyayolu != "")
+            {
+                thr = new Thread(WaitForDevices);
+                thr.IsBackground = true;
+                thr.Start();
+                btnInstall.Enabled = false;
+            }
+        }
+    }
+}
